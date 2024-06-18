97b173465bf92a28d4cc6329d444006ef3772d1b
Thanakorn Buathongtanakarn <scientan@gmail.com>
2024-06-18 18:31:56 +0700

    Update SCI31121AS1-2_Key.c

--- Assignment1/SCI31121AS1-2_Key.c
+++ Assignment1/SCI31121AS1-2_Key.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-float main() {
+int main() {
     //input
     float price;
+    float options;
+    printf("Enter teacher post-tax rate 0.93 or 1.07: ");
+    scanf("%f", &options);
     printf("Enter the price include vat: ");
     scanf("%f", &price);
 
     //Processing
-    float priceWithoutVat = price / 1.07;
+    float priceWithoutVat = price / options;
 
     //Output Handle Ex
     printf("The price without vat is: %.2f", priceWithoutVat);
