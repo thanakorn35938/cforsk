1dcfd89139b3796f8fc933a312a35164b1017288
Thanakorn Buathongtanakarn <scientan@gmail.com>
2024-07-02 06:09:30 +0000

    Added Key fro Assignment 3

---
+++ Assignment03/SCI31121AS3_Key.c
@@ -0,0 +1,21 @@
+#import <stdio.h>
+
+int main() {
+    /*Variable define*/
+    int k;
+    /*Recive age inoput from user*/
+    printf("Enter your age: ");
+    scanf("%d", &k);
+
+    /*Check if input is greater than 15 but not more than 90*/
+    if (k > 15 && k <= 90) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+
+    /*Program Ended*/
+    printf("Program Ended\n");
+    print("This program is eritten by Thanakorn Buathongtanakarn. Press any key to exit.\n")
+
+}
