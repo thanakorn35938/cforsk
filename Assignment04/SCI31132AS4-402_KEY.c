767d410830fe8f19cc79221acbb41232a4d25b87
Thanakorn Buathongtanakarn <scientan@gmail.com>
2024-07-23 14:56:01 +0700

    Update Assignment04
    Patch 402 Notice.

---
+++ Assignment04/SCI31132AS4-402_KEY.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int unit;
+    float price = 0.0;
+
+    printf("Enter the number of units: ");
+    scanf("%d", &unit);
+
+    if(unit < 50){
+        price = 20;
+    } else if (unit >= 50 && unit < 60){
+        price = unit * 5.25;
+    } else if (unit >= 60 && unit < 70){
+        price = unit * 6.25;
+    } else if (unit >= 70 && unit < 80){
+        price = unit * 7.25;
+    } else if (unit >= 80 && unit < 90){
+        price = unit * 8.25;
+    } else if (unit >= 90 && unit < 100){
+        price = unit * 9.25;
+    } else if (unit >= 100){
+        price = unit * 10.25;
+        printf("The price is %.2f\n", price);
+        printf("OVERLOAD");
+        return 0; // Ensure the function exits after handling the overload case
+    }
+
+    // Print the price for units less than 100
+    printf("The price is %.2f\n", price);
+    printf("NORMAL");
+}
