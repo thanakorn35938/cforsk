a708699017da60dcfdc8b520222d3e41e48261f0
Thanakorn Buathongtanakarn <scientan@gmail.com>
2024-07-30 06:54:24 +0000

    Added 402

---
+++ Assignment05/SCI31121AS05-402_Key.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    float height1, height2, height3, height4, height5;
+    float shortest;
+
+    printf("Enter the height of person 1: ");
+    scanf("%f", &height1);
+    printf("Enter the height of person 2: ");
+    scanf("%f", &height2);
+    printf("Enter the height of person 3: ");
+    scanf("%f", &height3);
+    printf("Enter the height of person 4: ");
+    scanf("%f", &height4);
+    printf("Enter the height of person 5: ");
+    scanf("%f", &height5);
+
+    shortest = height1;
+
+    if (height2 < shortest) {
+        shortest = height2;
+    }
+    if (height3 < shortest) {
+        shortest = height3;
+    }
+    if (height4 < shortest) {
+        shortest = height4;
+    }
+    if (height5 < shortest) {
+        shortest = height5;
+    }
+
+    printf("The shortest person has a height of %.2f\n", shortest);
+
+    return 0;
+}
