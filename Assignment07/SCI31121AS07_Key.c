36a322f5c81d18514f3eb8ae1f559e0eb703268b
Thanakorn Buathongtanakarn <scientan@gmail.com>
2024-08-13 06:00:42 +0000

    Added Assignment07

--- Assignment07/SCI31121AS07_Key.c
+++ Assignment07/SCI31121AS07_Key.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main() {
     //Write code here.
+    int num, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (int i = 1; i <= num; i += 2) {
+        sum += i;
+    }
+
+    printf("Sum of odd numbers from 1 to %d is %d\n", num, sum);
     return 0;
 }
 
