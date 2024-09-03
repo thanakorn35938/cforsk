35c82fd104a0d1d2db320d83bb93b0a8403e4922
Thanakorn Buathongtanakarn <scientan@gmail.com>
2024-09-03 14:08:38 +0700

    Added template for SCI31121AS09-402_Key.c

---
+++ Assignment09/SCI31121AS09-402_Key.c
@@ -0,0 +1,15 @@
+ #import <stdio.h>
+
+//Function prototypes
+float some_function (float variable);
+
+//Main function
+int main(){
+
+    return 0;
+}
+
+//Function definitions
+float some_function (float variable){
+    return 0;
+},
